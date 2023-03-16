791d6bc5c629e905ab34cc395c466125599e8633
Shailesh Aanand <anaandshailu@gmail.com>
2023-03-16 16:27:52 +0530

    L7

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (request, response) => {
+  const todos = await Todo.getTodos();
+  if (request.accepts("html")) {
+    response.render("index", {
+      todos,
+    });
+  } else {
+    response.json(todos);
+  }
 });
 
 app.get("/todos", async function (_, response) {
