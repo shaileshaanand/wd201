d65fac5087445e68616babf4b0a74663b5e53945
Shailesh Aanand <anaandshailu@gmail.com>
2023-04-12 17:39:30 +0530

    add user model

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -7,6 +7,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static associate(models) {
+      Todo.belongsTo(models.User, {
+        foreignKey: "userId",
+      });
+    }
 
     static addTodo({ title, dueDate, completed = false }) {
       return this.create({ title: title, dueDate, completed });
